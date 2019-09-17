0fedc97ac8f18d4949c029e33c1b90cf172cf52a
Josh Parrett <jtparrett@me.com>
2019-09-17 21:55:53 +0100

    expo web, netlify dev setup

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import gql from "graphql-tag";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components/native";
+
+const client = new ApolloClient({
+  uri: "/.netlify/functions/graphql"
+});
+
+const Title = styled.Text`
+  font-size: 60px;
+`;
+
+const Test = () => {
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+
+  if (loading) {
+    return <Text>Loading</Text>;
+  }
+
+  return (
+    <Title accessibilityRole="heading" aria-level="3">
+      {data.hello}
+    </Title>
+  );
+};
+
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Test />
+      </View>
+    </ApolloProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ functions/graphql/graphql.js
@@ -0,0 +1,27 @@
+const { ApolloServer, gql } = require("apollo-server-lambda");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Hello, world!";
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
