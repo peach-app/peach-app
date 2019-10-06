011fa2f911f73146231854eb8562e4a179da2603
Josh Parrett <jtparrett@me.com>
2019-10-06 14:45:52 +0100

    splash and app icon

--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -1,38 +1,41 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ImageBackground } from 'react-native';
 
 import { Main, Content, Actions } from './styles';
 import Button from '../../components/Button';
 import Container from '../../components/Container';
 import { Grid, GridItem } from '../../components/Grid';
+import splash from '../../assets/splash.png';
 
 const Landing = ({ navigation }) => (
-  <Main>
-    <Content>
-      <SafeAreaView>
-        <Container>
-          <Actions>
-            <Grid>
-              <GridItem>
-                <Button
-                  isDark
-                  title="Login"
-                  onPress={() => navigation.navigate('Login')}
-                />
-              </GridItem>
-              <GridItem>
-                <Button
-                  isDark
-                  title="Create an account"
-                  onPress={() => navigation.navigate('Register')}
-                />
-              </GridItem>
-            </Grid>
-          </Actions>
-        </Container>
-      </SafeAreaView>
-    </Content>
-  </Main>
+  <ImageBackground resizeMode="contain" source={splash} style={{ flex: 1 }}>
+    <Main>
+      <Content>
+        <SafeAreaView>
+          <Container>
+            <Actions>
+              <Grid>
+                <GridItem>
+                  <Button
+                    isDark
+                    title="Login"
+                    onPress={() => navigation.navigate('Login')}
+                  />
+                </GridItem>
+                <GridItem>
+                  <Button
+                    isDark
+                    title="Create an account"
+                    onPress={() => navigation.navigate('Register')}
+                  />
+                </GridItem>
+              </Grid>
+            </Actions>
+          </Container>
+        </SafeAreaView>
+      </Content>
+    </Main>
+  </ImageBackground>
 );
 
 export default Landing;
--- src/screens/Landing/styles.js
+++ src/screens/Landing/styles.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
+import * as Animatable from 'react-native-animatable';
 
 export const Main = styled.View`
   flex: 1;
 `;
 
-export const Content = styled.View`
+export const Content = styled(Animatable.View).attrs({
+  animation: 'slideInUp',
+  useNativeDriver: true,
+  duration: 300,
+})`
   background: ${props => props.theme.brand};
   border-top-left-radius: ${props => props.theme.radius}px;
   border-top-right-radius: ${props => props.theme.radius}px;
