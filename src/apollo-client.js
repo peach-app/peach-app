08e1e08fc8c1f369a4a335207bc28fb7bf72cd1f
Josh Parrett <jtparrett@me.com>
2019-10-08 16:54:53 +0100

    update public token

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -10,7 +10,7 @@ const client = new ApolloClient({
       headers: {
         authorization: token
           ? `Bearer ${token}`
-          : 'Bearer fnADaMjf0uACAMDRHuW3EkwWccroI-DqBfDX4wGr',
+          : 'Bearer fnADaM16XTACAIJOni96OZSU_nNMPP_qKX37lAHQ',
       },
     });
   },
