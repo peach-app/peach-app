4935787c26a5ad6099f93916811cd4e18c8a77c7
rosendanew <rosendanew@gmail.com>
2019-10-23 21:42:16 +0100

    Created new create_user function

---
+++ fauna/functions/create_user.js
@@ -0,0 +1,16 @@
+const { client, q } = require('../helpers/db');
+const { makeFunction } = require('../helpers/updateOrCreate');
+
+
+module.exports = async () => {
+    await client.query(
+        makeFunction({
+            name: 'create_user',
+            body: q.Query(
+                q.Lambda(
+                  ["email", "password"],
+                  q.Create(q.Collection("User"), { data: { email: q.Var("email"), password: q.Var("password") }}))),
+        })
+    );
+    console.log('"create_user" function created');
+}
