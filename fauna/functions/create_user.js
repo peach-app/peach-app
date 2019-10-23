2fab1eb4324398a9ec3514e31965da052bf8152f
Josh Parrett <jtparrett@me.com>
2019-10-23 22:12:48 +0100

    mode credentials into body of query

--- fauna/functions/create_user.js
+++ fauna/functions/create_user.js
@@ -1,16 +1,22 @@
 const { client, q } = require('../helpers/db');
 const { makeFunction } = require('../helpers/updateOrCreate');
 
-
 module.exports = async () => {
-    await client.query(
-        makeFunction({
-            name: 'create_user',
-            body: q.Query(
-                q.Lambda(
-                  ["email", "password"],
-                  q.Create(q.Collection("User"), { data: { email: q.Var("email"), credentials: { password: q.Var("password") } }}))),
-        })
-    );
-    console.log('"create_user" function created');
-}
+  await client.query(
+    makeFunction({
+      name: 'create_user',
+      body: q.Query(
+        q.Lambda(
+          ['email', 'password'],
+          q.Create(q.Collection('User'), {
+            data: {
+              email: q.Var('email'),
+            },
+            credentials: { password: q.Var('password') },
+          })
+        )
+      ),
+    })
+  );
+  console.log('"create_user" function created');
+};
