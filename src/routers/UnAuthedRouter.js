ffcecfe995ef2c2c388184173366fefc7b67666e
rosendanew <rosendanew@gmail.com>
2019-10-29 20:56:35 +0000

    Added the new screen to the UnAutherRouter

--- src/routers/UnAuthedRouter.js
+++ src/routers/UnAuthedRouter.js
@@ -3,11 +3,13 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import Landing from '../screens/Landing';
 import Login from '../screens/Login';
+import SignUp from '../screens/SignUp';
 
 const UnAuthedNavigator = createStackNavigator(
   {
     Landing,
     Login,
+    SignUp,
   },
   {
     headerMode: 'none',
