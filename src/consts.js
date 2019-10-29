e519f9170458a443b78c948cbbd915d231b07f1b
rosendanew <rosendanew@gmail.com>
2019-10-29 20:56:14 +0000

    Added CONSTS for form messages

--- src/consts.js
+++ src/consts.js
@@ -2,3 +2,9 @@ export const NETWORK_STATUS = {
   FETCHING: 1,
   REFETCHING: 4,
 };
+
+export const FORM_ERROR_MESSAGES = {
+  INVALID_EMAIL: "Please enter a valid email",
+  REQUIRED_PASSWORD: "We need you to set a password" ,
+  REQUIRED_EMAIL: "We need your email",
+}
