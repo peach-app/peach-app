18db761067957019b6dcd8597763d160d23bae90
rosendanew <rosendanew@gmail.com>
2019-11-04 20:49:34 +0000

    Added logic to return the secret after creating user

--- fauna/functions/register.js
+++ fauna/functions/register.js
@@ -5,19 +5,40 @@ module.exports = async () => {
   await client.query(
     makeFunction({
       name: 'register',
-      body: q.Query(
-        q.Lambda(
-          ['email', 'password', 'type'],
-          q.Create(q.Collection('User'), {
-            data: {
-              email: q.Var('email'),
-              type: q.Var('type'),
-            },
-            credentials: { password: q.Var('password') },
-          })
+      body: q.Do(
+        q.Query(
+          q.Lambda(
+            ['email', 'password', 'type'],
+            q.Create(q.Collection('User'), {
+              data: {
+                email: q.Var('email'),
+                type: q.Var('type'),
+              },
+              credentials: { password: q.Var('password') },
+            })
+          )
+        ),
+        q.Query(
+          q.Lambda(
+            ['email', 'password', 'type'],
+            q.Let(
+              {
+                user: q.Login(
+                  q.Match(q.Index('unique_User_email'), q.Var('email')),
+                  {
+                    password: q.Var('password'),
+                  }
+                ),
+              },
+              {
+                secret: q.Select(['secret'], q.Var('user')),
+              }
+            )
+          )
         )
       ),
     })
   );
+
   console.log('"register" function created');
 };
