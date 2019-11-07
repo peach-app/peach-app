48e0c3fc02f3f0daa3fa340cd7e83c74ddb83eaa
Josh Parrett <jtparrett@me.com>
2019-11-07 16:41:05 +0000

    web fix

--- src/screens/Thread/styles.js
+++ src/screens/Thread/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
 export const Composer = styled.View`
   padding-vertical: ${props => props.theme.spacingSmall}px;
