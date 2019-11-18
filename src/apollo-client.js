b002ec75a18d4231f71750f3e26cf127a86c4812
Josh Parrett <jtparrett@me.com>
2019-11-18 21:54:24 +0000

    apollo client link-rest

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,19 +1,36 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { RestLink } from 'apollo-link-rest';
+import { setContext } from 'apollo-link-context';
 import { AsyncStorage } from 'react-native';
 
-const client = new ApolloClient({
-  uri: 'https://graphql.fauna.com/graphql',
-  request: async operation => {
-    const token = await AsyncStorage.getItem('token');
+const cache = new InMemoryCache();
 
-    operation.setContext({
-      headers: {
-        authorization: token
-          ? `Bearer ${token}`
-          : 'Bearer fnADcuFlk7ACAC2eAW2YzfTZiA9vbQZ5-caS0x6Q',
-      },
-    });
-  },
+const httpLink = new HttpLink({
+  uri: 'https://graphql.fauna.com/graphql',
+});
+
+const restLink = new RestLink({
+  uri: 'https://dashboard.peachapp.io/.netlify/functions',
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await AsyncStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token
+        ? `Bearer ${token}`
+        : 'Bearer fnADcuFlk7ACAC2eAW2YzfTZiA9vbQZ5-caS0x6Q',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  cache,
+  link: authLink.concat(restLink.concat(httpLink)),
 });
 
 export default client;
