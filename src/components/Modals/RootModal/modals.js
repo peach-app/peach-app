c34c4d1dadb801591898ff1f04e4d8481817e2a1
rosendanew <rosendanew@gmail.com>
2019-11-25 22:39:18 +0000

    Added a modals file in which we will start adding the modules of the app

---
+++ src/components/Modals/RootModal/modals.js
@@ -0,0 +1,6 @@
+import CampaignCreationModal from '../CampaignCreationModal';
+import { MODAL_TYPES } from '../../../consts';
+
+export default {
+  [MODAL_TYPES.CAMPAIGN_CREATION]: CampaignCreationModal,
+};
