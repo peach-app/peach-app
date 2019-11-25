0d44ff3ce335d5e967d7b03d984da890f6bd4440
rosendanew <rosendanew@gmail.com>
2019-11-25 22:28:45 +0000

    Added logic for when a campaign is successfully created

--- src/screens/CreateCampaign/index.js
+++ src/screens/CreateCampaign/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
 import { Formik } from 'formik';
 import { useMutation } from '@apollo/react-hooks';
 import SafeAreaView from '../../components/SafeAreaView';
@@ -11,31 +12,37 @@ import Button from '../../components/Button';
 import Container from '../../components/Container';
 import Tabs from '../../components/Tabs';
 import DatePicker from '../../components/DatePicker';
-import { CAMPAIGN_TYPE } from '../../consts';
+import { CAMPAIGN_TYPE, MODAL_TYPES } from '../../consts';
 import CREATE_CAMPAIGN_MUTATION from './graphql/create-campaign';
-import { validationSchema } from './consts';
+import { validationSchema, FORM_INITIAL_VALUES } from './consts';
+import ModalContext from '../../contexts/Modal';
 
-
-const CreateCampaign = props => {
+const CreateCampaign = ({ navigation }) => {
   const [activeTab, setTab] = useState(0);
 
-  const [createCampaign, { loading, error }] = useMutation(
-    CREATE_CAMPAIGN_MUTATION,
-    {
-      onCompleted: data => console.log('DATA', data),
-    }
-  );
+  const { openModal } = useContext(ModalContext);
+
+  const [createCampaign, { loading }] = useMutation(CREATE_CAMPAIGN_MUTATION, {
+    // TO DO, HOW ARE WE GOING TO HANDLE ERRORS?
+    onError: err => console.log('errro', err),
+    onCompleted: () =>
+      openModal({
+        type: MODAL_TYPES.CAMPAIGN_CREATION,
+        props: {
+          onButtonClick: () => {
+            navigation.navigate('Campaigns', {
+              shouldRefetchQuery: true,
+            });
+          },
+        },
+      }),
+  });
 
   return (
     <Formik
       validateOnBlur={false}
       validateOnChange={false}
-      initialValues={{
-        name: '',
-        description: '',
-        budget: '',
-        dueDate: '',
-      }}
+      initialValues={FORM_INITIAL_VALUES}
       validationSchema={validationSchema}
       onSubmit={({ name, description, budget, dueDate }) => {
         createCampaign({
@@ -92,5 +99,8 @@ const CreateCampaign = props => {
     </Formik>
   );
 };
-
+CreateCampaign.propTypes = {
+  // eslint-disable-next-line
+  navigation: PropTypes.object,
+};
 export default CreateCampaign;
