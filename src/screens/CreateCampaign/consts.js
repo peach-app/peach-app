a05112b8f52d208fee903b031c92e5dbf510b2f5
rosendanew <rosendanew@gmail.com>
2019-11-25 22:29:00 +0000

    Added more consts

--- src/screens/CreateCampaign/consts.js
+++ src/screens/CreateCampaign/consts.js
@@ -1,16 +1,18 @@
 import * as Yup from 'yup';
-import { FORM_ERROR_MESSAGES } from '../../consts'
-
-
+import { FORM_ERROR_MESSAGES } from '../../consts';
 
 export const validationSchema = Yup.object().shape({
-    name: Yup.string().required(FORM_ERROR_MESSAGES.REQUIRED_FIELD),
-    description: Yup.string().required(FORM_ERROR_MESSAGES.REQUIRED_FIELD),
-    budget: Yup.number()
-      .required(FORM_ERROR_MESSAGES.REQUIRED_FIELD)
-      .positive(),
-    dueDate: Yup.string().required(FORM_ERROR_MESSAGES.REQUIRED_FIELD),
-  });
+  name: Yup.string().required(FORM_ERROR_MESSAGES.REQUIRED_FIELD),
+  description: Yup.string().required(FORM_ERROR_MESSAGES.REQUIRED_FIELD),
+  budget: Yup.number()
+    .required(FORM_ERROR_MESSAGES.REQUIRED_FIELD)
+    .positive(),
+  dueDate: Yup.string().required(FORM_ERROR_MESSAGES.REQUIRED_FIELD),
+});
 
-
-  
+export const FORM_INITIAL_VALUES = {
+  name: '',
+  description: '',
+  budget: '',
+  dueDate: '',
+};
