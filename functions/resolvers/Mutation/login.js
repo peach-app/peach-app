daa2038dacd29a2131a414f1dc9fde63f1e1c158
rosendanew <rosendanew@gmail.com>
2019-12-12 22:27:37 +0000

    Removed some console logs

--- functions/resolvers/Mutation/login.js
+++ functions/resolvers/Mutation/login.js
@@ -1,11 +1,9 @@
 module.exports = async (root, args, { client, q }) => {
   const { password } = args;
   const email = args.email.toLowerCase();
-  const z = await client.query(
+  return client.query(
     q.Login(q.Match(q.Index('user_by_email'), email), {
       password,
     })
   );
-  console.log('ssss', z);
-  return z;
 };
