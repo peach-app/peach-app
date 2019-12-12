7a26db551c62f189d107a90e163ca041918df179
rosendanew <rosendanew@gmail.com>
2019-12-12 22:27:50 +0000

    Fixed colors

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { View } from 'react-native';
 import Modal from 'react-native-modal';
+import { withTheme } from 'styled-components/native';
 
-export default ({ isOpen, onClose, shouldCloseOnBackdropClick, children }) => (
+const ModalComponent = ({
+  isOpen,
+  onClose,
+  shouldCloseOnBackdropClick,
+  children,
+  theme,
+}) => (
   <Modal
     isVisible={isOpen}
     animation="slideInUp"
     animationOut="slideOutDown"
     onBackdropPress={shouldCloseOnBackdropClick && onClose}
     style={{ justifyContent: 'flex-end', margin: 0 }}
+    backdropColor={theme.foreground}
   >
     <View
       style={{
-        backgroundColor: 'white',
+        backgroundColor: theme.background,
         paddingHorizontal: 20,
         paddingVertical: 50,
         borderRadius: 4,
-        borderColor: 'rgba(0, 0, 0, 0.1)',
+        borderColor: 'white',
       }}
     >
       {children}
     </View>
   </Modal>
 );
+
+export default withTheme(ModalComponent);
