7e80d65c54bf326a781dcd992f33f60c83b1be25
Josh Parrett <jtparrett@me.com>
2020-02-18 14:50:41 +0000

    add web only index

--- App.js
+++ App.js
@@ -1,7 +1,3 @@
-import 'react-native-gesture-handler';
-import { enableScreens } from 'react-native-screens';
 import App from './src';
-
-enableScreens();
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,84 +1,15 @@
-import React, { useState } from 'react';
-import { AppLoading } from 'expo';
-import { ApolloProvider } from '@apollo/react-hooks';
-import styled from 'styled-components/native';
-import { AppearanceProvider } from 'react-native-appearance';
-import * as Font from 'expo-font';
-import get from 'lodash/fp/get';
+import React from 'react';
+import 'react-native-gesture-handler';
+import { enableScreens } from 'react-native-screens';
+
 import { NavigationContainer } from '@react-navigation/native';
 
-import { Splash } from 'components';
-import { Provider as AuthProvider, useAuth } from 'contexts/Auth';
-import { Provider as UserProvider, useUser } from 'contexts/User';
+import { App } from './App';
 
-import ThemeProvider from './theme-provider';
-import client from './apollo-client';
-
-import {
-  UnAuthedNavigator,
-  OnboardingNavigator,
-  AuthedNavigator,
-} from './routers';
-
-const AuthedApp = () => {
-  const { user, loading } = useUser();
-
-  if (loading) {
-    return <Splash />;
-  }
-
-  if (!get('user.onboarded', user)) {
-    return <OnboardingNavigator />;
-  }
-
-  return <AuthedNavigator />;
-};
-
-const App = () => {
-  const [loading, setLoading] = useState(true);
-  const { isLoggedIn } = useAuth();
-
-  if (loading) {
-    return (
-      <AppLoading
-        startAsync={async () =>
-          Font.loadAsync({
-            'futura-bold': require('./assets/futura-bold.ttf'),
-            'futura-book': require('./assets/futura-book.ttf'),
-          })
-        }
-        onFinish={() => setLoading(false)}
-      />
-    );
-  }
-
-  if (isLoggedIn) {
-    return (
-      <UserProvider>
-        <AuthedApp />
-      </UserProvider>
-    );
-  }
-
-  return <UnAuthedNavigator />;
-};
-
-const Main = styled.View`
-  flex: 1;
-`;
+enableScreens();
 
 export default () => (
   <NavigationContainer>
-    <AuthProvider>
-      <ApolloProvider client={client}>
-        <AppearanceProvider>
-          <ThemeProvider>
-            <Main>
-              <App />
-            </Main>
-          </ThemeProvider>
-        </AppearanceProvider>
-      </ApolloProvider>
-    </AuthProvider>
+    <App />
   </NavigationContainer>
 );
