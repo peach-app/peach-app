d7eafa17dea2c8847b99851bdc384ca6e89660d1
Josh Parrett <jtparrett@me.com>
2020-02-19 13:36:12 +0000

    registration email

--- functions/resolvers/Mutation/register.js
+++ functions/resolvers/Mutation/register.js
@@ -1,8 +1,22 @@
+const transporter = require('../../helpers/mailTransporter');
+
+const registrationEmail = ({ name }) => `
+Hi ${name},
+
+Thank you for registering to use Peach.
+
+If you have any questions feel free to email us at peachapp.io@gmail.com.
+
+Thanks,
+The Peach Team
+https://peachapp.io
+`;
+
 module.exports = async (root, args, { client, q }) => {
   const { name, password, type } = args;
   const email = args.email.toLowerCase();
 
-  return client.query(
+  const result = await client.query(
     q.If(
       q.Exists(q.Match(q.Index('user_by_email'), email)),
       q.Abort('A user with this email address already exists.'),
@@ -21,4 +35,22 @@ module.exports = async (root, args, { client, q }) => {
       )
     )
   );
+
+  transporter.sendMail(
+    {
+      from: 'peachapp.io@gmail.com',
+      to: email,
+      subject: 'Welcome to Peach!',
+      text: registrationEmail({ name }),
+    },
+    (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    }
+  );
+
+  return result;
 };
