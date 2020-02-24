9ae7f474aa3662d702708379475d1b7461b6e8c1
rosendanew <rosendanew@gmail.com>
2020-02-24 23:38:48 +0000

    added formatter helper

--- src/screens/RequestInfluencers/helper.js
+++ src/screens/RequestInfluencers/helper.js
@@ -1,15 +1,19 @@
-export const formatInfluencersArray = (requestedInfluencers, actionedInfluencer) => {
-
-    if (requestedInfluencers.length > 0) {
-  
-      if (requestedInfluencers.find(influencer => influencer._id === actionedInfluencer._id)) {
-        return requestedInfluencers.filter(influencer => influencer._id !== actionedInfluencer._id);
-      }
-      return [...requestedInfluencers, actionedInfluencer];
-  
+export const formatInfluencersArray = (
+  requestedInfluencers,
+  actionedInfluencer
+) => {
+  if (requestedInfluencers.length > 0) {
+    if (
+      requestedInfluencers.find(
+        influencer => influencer._id === actionedInfluencer._id
+      )
+    ) {
+      return requestedInfluencers.filter(
+        influencer => influencer._id !== actionedInfluencer._id
+      );
     }
-  
-  
-    return [actionedInfluencer];
-  
-  };
+    return [...requestedInfluencers, actionedInfluencer];
+  }
+
+  return [actionedInfluencer];
+};
