469e680b4a9c9d68340f6f89e4864fbf0044e49e
rosendanew <rosendanew@gmail.com>
2020-02-24 23:38:34 +0000

    Refactored

--- src/screens/RequestInfluencers/RequestedInfluencers/index.js
+++ src/screens/RequestInfluencers/RequestedInfluencers/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import get from 'lodash/fp/get';
 import { Text, Grid, Avatar } from '../../../components';
-import { RequestedInfluencersWrapper, RequestedInfluencersList } from './styles';
+import { RequestedInfluencersWrapper } from './styles';
 
-const RequestedInfluencers = ({requestedInfluencers}) =>
-<RequestedInfluencersWrapper>
-<Grid>
-<Grid.Item size={12}>
-<Text>Influencers bag</Text>
-</Grid.Item>
-<Grid.Item size={12}>
-<Avatar.List>
-{requestedInfluencers.map(influencer => <Avatar fallback={influencer.name} source={{uri : get('avatar.url', influencer)}} />)}
-</Avatar.List> 
-</Grid.Item>
+const RequestedInfluencers = ({ requestedInfluencers }) => (
+  <RequestedInfluencersWrapper>
+    <Grid>
+      <Grid.Item size={12}>
+        <Text>Influencers bag</Text>
+      </Grid.Item>
+      <Grid.Item size={12}>
+        <Avatar.List>
+          {requestedInfluencers.map(influencer => (
+            <Avatar
+              fallback={influencer.name}
+              source={{ uri: get('avatar.url', influencer) }}
+            />
+          ))}
+        </Avatar.List>
+      </Grid.Item>
+    </Grid>
+  </RequestedInfluencersWrapper>
+);
 
-</Grid>
-
-
-</RequestedInfluencersWrapper>
-
+RequestedInfluencers.propTypes = {
+  requestedInfluencers: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      avatar: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+    })
+  ).isRequired,
+};
 export default RequestedInfluencers;
--- src/screens/RequestInfluencers/RequestedInfluencers/styles.js
+++ src/screens/RequestInfluencers/RequestedInfluencers/styles.js
@@ -1,14 +1,5 @@
 import styled from 'styled-components';
 
-
 export const RequestedInfluencersWrapper = styled.View`
   padding: ${props => props.theme.spacing}px;
 `;
-
-export const RequestedInfluencersList = styled.View`
-  flex-direction: row-reverse;
-  justify-content: flex-end;
-  align-items: flex-end;
-`;
-
-
