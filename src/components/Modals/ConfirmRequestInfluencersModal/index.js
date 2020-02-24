4f5700e0c777147536c296e68a05ab54974e8506
rosendanew <rosendanew@gmail.com>
2020-02-24 23:32:26 +0000

    added ConfirmRequestInfluencers modal

--- src/components/Modals/ConfirmRequestInfluencersModal/index.js
+++ src/components/Modals/ConfirmRequestInfluencersModal/index.js
@@ -1,56 +1,108 @@
-import React from 'react';
-import {  useMutation } from '@apollo/react-hooks';
-import get from 'lodash/fp/get';
-import styled from 'styled-components';
-import REQUEST_INFLUENCERS from './graphql'
-import { Title, SubTitle, Modal, Icon, Grid, Button, Avatar, Actions } from '../../'
+import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { useMutation } from '@apollo/react-hooks';
+import { get } from 'lodash/fp';
+import REQUEST_INFLUENCERS from './graphql';
+import { Title, SubTitle, Modal, Grid, Button, Avatar, Actions } from '../..';
+import FeedbackView from '../../FeedbackView';
 
+const ConfirmRequestedInfluencersModal = ({
+  onClose,
+  requestedInfluencers,
+  campaignId,
+  navigation,
+}) => {
+  const [isSuccessScreenVisible, setSuccessScreen] = useState(false);
 
-export default ({onClose, requestedInfluencers, campaignId}) => 
-{
-  const [requestInfluencers, { loading }] = useMutation(REQUEST_INFLUENCERS, {
-    onCompleted: (data) => console.log("DATAAA", data)
-  });
+  const [requestInfluencers, { loading, error }] = useMutation(
+    REQUEST_INFLUENCERS,
+    {
+      onCompleted: () => setSuccessScreen(true),
+    }
+  );
 
-  const onSubmitRequest = () => {
-    console.log('INN');
-    requestInfluencers({
-      variables: {
-        campaignId,
-        requestedInfluencers
-      }
-    })
+  if (error) {
+    // SET FEEDBACK VIEW WITH ERRORS AND HANDLE RETRY
   }
 
-  return (
+  const onSubmitRequest = () => {
+    requestInfluencers({
+      variables: {
+        requestedInfluencers: requestedInfluencers.map(
+          influencer => influencer._id
+        ),
+        campaignId,
+      },
+    });
+  };
 
-  
-      <Modal isOpen onClose={onClose} shouldCloseOnBackdropClick>
-        <Grid justify="center" align="center">
-          
-          <Grid.Item size={12}>
-            <Title isCentered>Great choice!</Title>
-          </Grid.Item>
-          <Grid.Item size={12}>
-            <SubTitle isCentered>
-              You are about to request the following influencers:
-            </SubTitle>
-          </Grid.Item>
-          <Grid.Item size={12}>
-<Avatar.List>
-{requestedInfluencers.map(influencer => <Avatar size={60} fallback={influencer.name} source={{uri : get('avatar.url', influencer)}} />)}
-</Avatar.List> 
-</Grid.Item>
-          <Grid.Item size={12}>
-          <Actions>
-                  <Button
-                      onPress={onSubmitRequest}
-                    title="Request"
-                    fixedWidth
+  const handleFeedbackActionButtonPressed = useCallback(() => {
+    onClose();
+    // ADD REFRESH PARAM
+    navigation.navigate('Campaigns');
+  }, []);
+
+  return (
+    <Modal isOpen onClose={onClose} shouldCloseOnBackdropClick>
+      <Grid justify="center" align="center">
+        {isSuccessScreenVisible ? (
+          <FeedbackView
+            title="Influecers successfully requested!"
+            subTitle="They will be notified about your interest"
+            actionButtonLabel="Go back"
+            onActionButtonPressed={handleFeedbackActionButtonPressed}
+          />
+        ) : (
+          <>
+            <Grid.Item size={12}>
+              <Title isCentered>Great choice!</Title>
+            </Grid.Item>
+            <Grid.Item size={12}>
+              <SubTitle isCentered>
+                You are about to request the following influencers:
+              </SubTitle>
+            </Grid.Item>
+            <Grid.Item size={12}>
+              <Avatar.List isCentered>
+                {requestedInfluencers.map(influencer => (
+                  <Avatar
+                    size={60}
+                    fallback={influencer.name}
+                    source={{ uri: get('avatar.url', influencer) }}
                   />
-                  </Actions>
-              </Grid.Item>
-        </Grid>
-      </Modal>
-  )
+                ))}
+              </Avatar.List>
+            </Grid.Item>
+            <Grid.Item size={12}>
+              <Actions>
+                <Button
+                  onPress={onSubmitRequest}
+                  title="Request"
+                  fixedWidth
+                  isLoading={loading}
+                />
+              </Actions>
+            </Grid.Item>
+          </>
+        )}
+      </Grid>
+    </Modal>
+  );
 };
+
+ConfirmRequestedInfluencersModal.propTypes = {
+  requestedInfluencers: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      avatar: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+    })
+  ).isRequired,
+  onClose: PropTypes.func.isRequired,
+  campaignId: PropTypes.string.isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
+export default ConfirmRequestedInfluencersModal;
