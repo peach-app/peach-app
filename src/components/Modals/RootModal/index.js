0e0e5fb1859bc630053a8405d296b4a346c8a413
rosendanew <rosendanew@gmail.com>
2020-02-24 23:33:08 +0000

    removed logs

--- src/components/Modals/RootModal/index.js
+++ src/components/Modals/RootModal/index.js
@@ -1,17 +1,16 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import { useModal } from '../../../contexts/Modal';
 import Modals from './modals';
 
 const RootModal = () => {
   const { modal, closeModal } = useModal();
-  
-  console.log('mod', modal)
+
   if (!modal.type) {
     return null;
   }
 
   const CurrentModal = Modals[modal.type];
-  return <CurrentModal onClose={() => console.log("YEE",closeModal) || closeModal()} {...modal.props} />;
+  return <CurrentModal onClose={() => closeModal()} {...modal.props} />;
 };
 
 export default RootModal;
