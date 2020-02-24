06e194c809f1ef35f150dfa19d5adaa5a0e602cc
rosendanew <rosendanew@gmail.com>
2020-02-24 23:40:28 +0000

    Added feedback view screen

---
+++ src/components/FeedbackView/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Grid, Icon, Title, Button, SubTitle, Actions } from '..';
+
+const IconWrapper = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+const FeedbackView = ({
+  title,
+  subTitle,
+  actionButtonLabel,
+  onActionButtonPressed,
+}) => (
+  <>
+    <Grid.Item size={12}>
+      {/* Better icon or SVG here. */}
+      <IconWrapper>
+        <Icon size={100} name="ios-checkmark" />
+      </IconWrapper>
+    </Grid.Item>
+    <Grid.Item size={12}>
+      <Title isCentered>{title}</Title>
+    </Grid.Item>
+    <Grid.Item size={12}>
+      <SubTitle isCentered>{subTitle}</SubTitle>
+    </Grid.Item>
+    <Grid.Item size={12}>
+      <Actions>
+        <Button
+          fixedWidth
+          title={actionButtonLabel}
+          onPress={onActionButtonPressed}
+        />
+      </Actions>
+    </Grid.Item>
+  </>
+);
+
+export default FeedbackView;
