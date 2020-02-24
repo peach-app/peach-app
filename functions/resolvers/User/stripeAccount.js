d02ee1e389a549040ecb593771a6fd41f780777a
Josh Parrett <jtparrett@me.com>
2020-02-24 19:34:35 +0000

    non stripe query for missing stripeID

--- functions/resolvers/User/stripeAccount.js
+++ functions/resolvers/User/stripeAccount.js
@@ -1,5 +1,9 @@
 const stripe = require('../../helpers/stripe');
 
 module.exports = (root, args) => {
+  if (!root.stripeID) {
+    return undefined;
+  }
+
   return stripe.accounts.retrieve(root.stripeID);
 };
