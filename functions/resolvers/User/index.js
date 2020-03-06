9564148e8e48240c3d786d3d02d534b2fdb7621e
rosendanew <rosendanew@gmail.com>
2020-03-06 16:12:01 +0000

    added emailVerificaiton type resolver

--- functions/resolvers/User/index.js
+++ functions/resolvers/User/index.js
@@ -2,4 +2,5 @@ module.exports = {
   campaigns: require('./campaigns'),
   threads: require('./threads'),
   stripeAccount: require('./stripeAccount'),
+  emailVerification: require('./emailVerification'),
 };
