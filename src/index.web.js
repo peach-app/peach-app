13d58443a7701fec23166f63100eb139f7df324d
rosendanew <rosendanew@gmail.com>
2020-03-06 16:13:26 +0000

    Extended web.js with route and some providers

--- src/index.web.js
+++ src/index.web.js
@@ -3,7 +3,9 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AppearanceProvider } from 'react-native-appearance';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { VerifyEmail } from './screens/VerifyEmail';
+import client from './apollo-client';
 import { App } from './App';
 
 import ThemeProvider from './theme-provider';
@@ -12,11 +14,13 @@ export default () => (
   <Router>
     <Switch>
       <Route path="/verify-email/:token">
-        <AppearanceProvider>
-          <ThemeProvider>
-            <VerifyEmail />
-          </ThemeProvider>
-        </AppearanceProvider>
+        <ApolloProvider client={client}>
+          <AppearanceProvider>
+            <ThemeProvider>
+              <VerifyEmail />
+            </ThemeProvider>
+          </AppearanceProvider>
+        </ApolloProvider>
       </Route>
       <Route path="/">
         <NavigationContainer>
