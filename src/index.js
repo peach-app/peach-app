6caead8b85690bb463639a54eacdbe26cb41ebfc
rosendanew <rosendanew@gmail.com>
2020-03-06 16:29:26 +0000

    reverted file

--- src/index.js
+++ src/index.js
@@ -2,54 +2,14 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { enableScreens } from 'react-native-screens';
 
-import { NavigationContainer, useLinking } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import { App } from './App';
 
 enableScreens();
 
-export default () => {
-  console.log('HEYYYYYY');
-  const ref = React.useRef();
-
-  const { getInitialState } = useLinking(ref, {
-    prefixes: ['192.168.1.118:19006'],
-  });
-
-  const [isReady, setIsReady] = React.useState(false);
-  const [initialState, setInitialState] = React.useState();
-
-  React.useEffect(() => {
-    console.log('EEEEEEEE');
-    Promise.race([
-      getInitialState(),
-      new Promise(resolve =>
-        // Timeout in 150ms if `getInitialState` doesn't resolve
-        // Workaround for https://github.com/facebook/react-native/issues/25675
-        setTimeout(resolve, 150)
-      ),
-    ])
-      .catch(e => {
-        console.error(e);
-      })
-      .then(state => {
-        if (state !== undefined) {
-          setInitialState(state);
-        }
-
-        setIsReady(true);
-      });
-  }, [getInitialState]);
-
-  if (!isReady) {
-    return null;
-  }
-
-  console.log('INITIAL STATE', initialState);
-
-  return (
-    <NavigationContainer initialState={initialState} ref={ref}>
-      <App />
-    </NavigationContainer>
-  );
-};
+export default () => (
+  <NavigationContainer>
+    <App />
+  </NavigationContainer>
+);
