395638fba141e6219b1cc2d097c23d8808ff075e
rosendanew <rosendanew@gmail.com>
2020-03-15 22:23:32 +0000

    removed unused Icon

--- src/components/CampaignCard/styles.js
+++ src/components/CampaignCard/styles.js
@@ -1,15 +1,7 @@
 import styled from 'styled-components/native';
-import { Ionicons } from '@expo/vector-icons';
 
 import { Text } from '../Text';
 import { Title } from '../Title';
-
-export const Icon = styled(Ionicons).attrs(props => ({
-  color: props.theme.foreground,
-  size: 22,
-}))`
-  margin-right: ${props => props.theme.spacingSmall}px;
-`;
 
 export const MainTitle = styled(Text)`
   margin-bottom: ${props => props.theme.spacingXSmall}px;
