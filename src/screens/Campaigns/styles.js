a9cc940f0830e606d15d08da2812465bf7e68a4c
rosendanew <rosendanew@gmail.com>
2020-03-15 22:28:58 +0000

    new styles with icon

---
+++ src/screens/Campaigns/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+import { Ionicons } from '@expo/vector-icons';
+
+export const Icon = styled(Ionicons).attrs(props => ({
+  color: props.theme.foreground,
+  size: 22,
+}))`
+  margin-right: ${props => props.theme.spacingSmall}px;
+`;
