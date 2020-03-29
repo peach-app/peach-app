53f4c86c08aa84c5fd5c80fa7fb2d06e9b1e36cf
rosendanew <rosendanew@gmail.com>
2020-03-29 20:23:38 +0100

    Extended WebView

---
+++ src/components/SocialAccounts/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Ionicons } from '@expo/vector-icons';
+
+const Icon = styled(Ionicons).attrs(props => ({
+  color: props.theme.foreground,
+  size: 32,
+}))``;
+
+const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-horizontal: ${props => props.theme.spacingLarge};
+`;
+
+const IconWrapper = styled.TouchableOpacity``;
+
+const ICON_PROPS = {
+  instagram: {
+    icon: 'logo-instagram',
+    url: 'https://www.instagram.com/{account}/',
+  },
+  twitter: {
+    icon: 'logo-twitter',
+    url: 'https://twitter.com/{account}',
+  },
+  facebook: {
+    icon: 'logo-facebook',
+    url: 'https://www.facebook.com/{account}',
+  },
+  youTube: {
+    icon: 'logo-youtube',
+    url: 'https://www.youtube.com/user/{account}/',
+  },
+  tikTok: { icon: 'logo-codepen', url: 'https://www.tiktok.com/@{account}?' },
+};
+
+export const SocialAccounts = ({ socialAccounts, onSocialAccountPressed }) => (
+  <Container>
+    {Object.keys(socialAccounts).map(
+      (account, i) =>
+        ICON_PROPS[account] && (
+          <IconWrapper
+            // eslint-disable-next-line
+          key={`${i}-${account}`}
+            onPress={() =>
+              onSocialAccountPressed(
+                ICON_PROPS[account].url.replace(
+                  '{account}',
+                  socialAccounts[account]
+                )
+              )
+            }
+          >
+            <Icon name={ICON_PROPS[account].icon} />
+          </IconWrapper>
+        )
+    )}
+  </Container>
+);
+
+SocialAccounts.propTypes = {
+  socialAccounts: PropTypes.object.isRequired,
+  onSocialAccountPressed: PropTypes.func.isRequired,
+};
---
+++ src/components/WebView/index.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { BackHandler } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { Header, Main, Icon } from './styles';
+
+const WebViewComponent = ({ source, onClose }) => {
+  const [canGoBack, setCanGoBack] = useState(false);
+  const webView = useRef(null);
+  const handleBackButton = () => {
+    if (canGoBack) {
+      webView.current.goBack();
+      return true;
+    }
+    return false;
+  };
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+
+    return () =>
+      BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+  }, []);
+
+  return (
+    <>
+      <Header>
+        <Main onPress={handleBackButton}>
+          <Icon name="ios-arrow-back" size={30} />
+        </Main>
+
+        <Main onPress={onClose}>
+          <Icon name="ios-close" size={40} />
+        </Main>
+      </Header>
+      <WebView
+        source={{ uri: source }}
+        ref={webView}
+        injectedJavaScript={`
+        (function() {
+          function wrap(fn) {
+            return function wrapper() {
+              var res = fn.apply(this, arguments);
+              window.ReactNativeWebView.postMessage('navigationStateChange');
+              return res;
+            }
+          }
+  
+          history.pushState = wrap(history.pushState);
+          history.replaceState = wrap(history.replaceState);
+          window.addEventListener('popstate', function() {
+            window.ReactNativeWebView.postMessage('navigationStateChange');
+          });
+        })();
+  
+        true;
+      `}
+        onMessage={({ nativeEvent: state }) => {
+          if (state.data === 'navigationStateChange') {
+            setCanGoBack(state.canGoBack);
+          }
+        }}
+      />
+    </>
+  );
+};
+
+WebViewComponent.propTypes = {
+  source: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+export default WebViewComponent;
