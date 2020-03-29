edde83b871012abf9c4b3d61e4e898f7ddcdde48
rosendanew <rosendanew@gmail.com>
2020-03-29 20:27:18 +0100

    Refactoring

--- src/components/Modals/RootModal/modals.js
+++ src/components/Modals/RootModal/modals.js
@@ -1,8 +1,10 @@
 import { MODAL_TYPES } from 'consts';
 import CampaignCreationModal from '../CampaignCreationModal';
 import ConfirmRequestInfluencersModal from '../ConfirmRequestInfluencersModal';
+import SocialAccountWebViewModal from '../SocialAccountWebViewModal';
 
 export default {
   [MODAL_TYPES.CAMPAIGN_CREATION]: CampaignCreationModal,
   [MODAL_TYPES.CONFIRM_REQUEST_INFLUENCERS]: ConfirmRequestInfluencersModal,
+  [MODAL_TYPES.SOCIAL_ACCOUNT_WEB_VIEW]: SocialAccountWebViewModal,
 };
--- src/components/index.js
+++ src/components/index.js
@@ -41,3 +41,5 @@ export * from './DatePicker';
 export * from './SearchInfluencers';
 export * from './AvatarUpload';
 export * from './MoneyInput';
+export * from './SocialAccounts';
+export * from './ProgressiveImage';
