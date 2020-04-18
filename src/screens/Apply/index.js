18306105eb7cff19af9e3eed2b84424164263ef4
Josh Parrett <jtparrett@me.com>
2020-04-18 10:33:25 +0100

    payrate min

--- src/screens/Apply/index.js
+++ src/screens/Apply/index.js
@@ -20,7 +20,9 @@ import {
 import APPLY_TO_CAMPAIGN from './graphql/apply-to-campaign';
 
 const validationSchema = Yup.object().shape({
-  cost: Yup.number().required('Please enter your pay rate for this campaign'),
+  cost: Yup.number()
+    .min(500, 'Pay rate must be more than £5.00')
+    .required('Please enter your pay rate for this campaign'),
 });
 
 export const Apply = () => {
