12014491fb218a8b6cbe7cf68721f20047ecfb9f
rosendanew <rosendanew@gmail.com>
2020-04-22 22:32:44 +0100

    Added PayoutCard

---
+++ src/components/PayoutCard/consts.js
@@ -0,0 +1,22 @@
+export const PAYOUT_STATUSES = {
+  succeeded: {
+    icon: 'ios-checkmark-circle-outline',
+    label: 'Paid',
+    color: 'green',
+  },
+  pending: {
+    icon: 'ios-timer',
+    label: 'Pending',
+    color: 'orange',
+  },
+  failed: {
+    icon: 'ios-close-circle-outline',
+    label: 'Failed',
+    color: 'error',
+  },
+  refund: {
+    icon: 'ios-swap',
+    label: 'Refund',
+    color: 'brand',
+  },
+};
