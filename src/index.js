872ca6709dcff756f18bd221b7bc3f290c2d22fb
rosendanew <rosendanew@gmail.com>
2020-05-14 12:19:49 +0100

    sentry init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'react-native-gesture-handler';
+import * as Sentry from 'sentry-expo';
 import { enableScreens } from 'react-native-screens';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,6 +8,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { App } from './App';
 
 enableScreens();
+
+Sentry.init({
+  dsn:
+    'https://05ee3234bab843a3a9fcac6890e3694a@o392600.ingest.sentry.io/5240413',
+  enableInExpoDevelopment: true,
+  debug: true,
+});
 
 export default () => (
   <NavigationContainer>
