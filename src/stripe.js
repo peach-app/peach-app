faee351dae4f49a72ed34425ed00280a53dbf42b
Josh Parrett <jtparrett@me.com>
2020-05-21 18:36:33 +0100

    change stripe test key

--- src/stripe.js
+++ src/stripe.js
@@ -1,7 +1,7 @@
 import stripeClient from 'stripe-client';
 
 const secret = {
-  development: 'pk_test_1N9AOYZFpC5gIU4DI9Rnd2DV009ew3jAaR',
+  development: 'pk_test_vMq0DMaRNOD813Osl9XS2uBu00HmG5yS4d',
   production: 'pk_live_W5F69iL2lXn93thCcqXevUIR00lVBUHDaq',
 }[process.env.NODE_ENV];
 
