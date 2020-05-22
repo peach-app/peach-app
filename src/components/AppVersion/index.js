210ab33389ba258a7b05a1150b758ead42511a9e
rosendanew <rosendanew@gmail.com>
2020-05-22 10:44:02 +0100

    styled web fix

--- src/components/AppVersion/index.js
+++ src/components/AppVersion/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 import Constants from 'expo-constants';
 import { Text } from '../Text';
 
