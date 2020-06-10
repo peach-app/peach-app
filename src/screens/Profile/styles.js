6f1fb0e4cb397961b057bdc170a77b63ec4b4898
Josh Parrett <jtparrett@me.com>
2020-06-10 17:36:28 +0100

    show sample work

--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -15,3 +15,19 @@ export const Section = styled.View`
 export const Categories = styled.View`
   margin-top: ${props => props.theme.spacingSmall}px;
 `;
+
+export const WorkSamples = styled.ScrollView.attrs(props => ({
+  marginHorizontal: -props.theme.spacing,
+  contentContainerStyle: {
+    padding: props.theme.spacing,
+    paddingRight: 0,
+  },
+  horizontal: true,
+}))``;
+
+export const Media = styled.Image`
+  height: 240px;
+  width: 240px;
+  border-radius: ${props => props.theme.radius}px;
+  margin-right: ${props => props.theme.spacing}px;
+`;
