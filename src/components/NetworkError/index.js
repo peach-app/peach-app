71e6da053c088980451129cdefc9009a4bda3e7c
Josh Parrett <jtparrett@me.com>
2020-06-16 10:34:33 +0100

    network safeareaview

--- src/components/NetworkError/index.js
+++ src/components/NetworkError/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
 
+import { SafeAreaView } from '../SafeAreaView';
 import { Title } from '../Title';
 import { Text } from '../Text';
 import { Button } from '../Button';
@@ -14,26 +15,28 @@ const Main = styled.View`
 `;
 
 export const NetworkError = ({ loading, refetch }) => (
-  <Main>
-    <Grid>
-      <Grid.Item size={12}>
-        <Title isCenter>Uh oh!</Title>
-      </Grid.Item>
-      <Grid.Item size={12}>
-        <Text isCenter>We're having trouble at this time.</Text>
-      </Grid.Item>
-      <Grid.Item size={12}>
-        <Actions>
-          <Button
-            title="Try again"
-            fixedWidth
-            isLoading={loading}
-            onPress={() => refetch()}
-          />
-        </Actions>
-      </Grid.Item>
-    </Grid>
-  </Main>
+  <SafeAreaView>
+    <Main>
+      <Grid>
+        <Grid.Item size={12}>
+          <Title isCenter>Uh oh!</Title>
+        </Grid.Item>
+        <Grid.Item size={12}>
+          <Text isCenter>We're having trouble at this time.</Text>
+        </Grid.Item>
+        <Grid.Item size={12}>
+          <Actions>
+            <Button
+              title="Try again"
+              fixedWidth
+              isLoading={loading}
+              onPress={() => refetch()}
+            />
+          </Actions>
+        </Grid.Item>
+      </Grid>
+    </Main>
+  </SafeAreaView>
 );
 
 NetworkError.defaultProps = {
